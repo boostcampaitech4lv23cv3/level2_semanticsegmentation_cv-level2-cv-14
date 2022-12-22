c6d33910827ea76b4d5efe6f1a67992e431a059a
GeonHyeock <gunheock@gmail.com>
2022-12-22 16:18:09 +0000

    Feat: copy paste aug
    copy paste aug기능을 추가

--- pytorch-hydra-template/src/dataset.py
+++ pytorch-hydra-template/src/dataset.py
@@ -7,8 +7,11 @@ import torchvision.transforms as transforms
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import json
-import os
+import os, sys
 from seed_everything import seedEverything, _init_fn
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+from copypaste import copy_paste
 
 # seed
 seedEverything(42)
@@ -82,6 +85,107 @@ class CustomDataSet(Dataset):
                 images = transformed["image"]
                 masks = transformed["mask"]
             return images, masks, image_infos
+
+        if self.mode == "test":
+            # transform -> albumentations 라이브러리 활용
+            if self.transform is not None:
+                transformed = self.transform(image=images)
+                images = transformed["image"]
+            return images, image_infos
+
+    def __len__(self) -> int:
+        # 전체 dataset의 size를 return
+        return len(self.coco.getImgIds())
+
+
+@copy_paste.copy_paste_class
+class CopyPasteDataSet(Dataset):
+    """COCO format"""
+
+    # mode: train, val, test
+
+    def __init__(self, data_dir, mode="train", transform=None):
+        super().__init__()
+        self.mode = mode
+        self.transform = transform
+        self.data_dir = data_dir
+        self.coco = COCO(data_dir + "/" + mode + ".json")
+        self.category_names = [
+            "Backgroud",
+            "General trash",
+            "Paper",
+            "Paper pack",
+            "Metal",
+            "Glass",
+            "Plastic",
+            "Styrofoam",
+            "Plastic bag",
+            "Battery",
+            "Clothing",
+        ]
+
+    def load_example(self, index: int):
+        # dataset이 index되어 list처럼 동작
+        image_id = self.coco.getImgIds(imgIds=index)
+        image_infos = self.coco.loadImgs(image_id)[0]
+        # cv2 를 활용하여 image 불러오기
+        images = cv2.imread(os.path.join(self.data_dir, image_infos["file_name"]))
+        images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB).astype(np.float32)
+        images /= 255.0
+
+        if self.mode in ("train", "val"):
+            ann_ids = self.coco.getAnnIds(imgIds=image_infos["id"])
+            anns = self.coco.loadAnns(ann_ids)
+            # Load the categories in a variable
+            cat_ids = self.coco.getCatIds()
+            cats = self.coco.loadCats(cat_ids)
+            # masks : size가 (height x width)인 2D
+            # 각각의 pixel 값에는 "category id" 할당
+            # Background = 0
+            masks = np.zeros((image_infos["height"], image_infos["width"]))
+            # General trash = 1, ... , Cigarette = 10
+            anns = sorted(anns, key=lambda idx: idx["area"], reverse=True)
+            bboxes = []
+            for i in range(len(anns)):
+                category_id = anns[i]["category_id"]
+                className = get_classname(category_id, cats)
+                pixel_value = self.category_names.index(className)
+                x, y, x2, y2 = anns[i]["bbox"]
+                bboxes.append([x, y, x2, y2, category_id, i])
+                masks[self.coco.annToMask(anns[i]) == 1] = pixel_value
+            masks = masks.astype(np.int8)
+
+            # copy_paste
+            # paste_id = self.coco.getImgIds(imgIds=index + 1)
+            # paste_infos = self.coco.loadImgs(paste_id)[0]
+            # paste_image = cv2.imread(
+            #     os.path.join(self.data_dir, paste_infos["file_name"])
+            # )
+            # paste_image = cv2.cvtColor(paste_image, cv2.COLOR_BGR2RGB).astype(
+            #     np.float32
+            # )
+            # paste_image /= 255.0
+            # ann_ids = self.coco.getAnnIds(imgIds=paste_infos["id"])
+            # anns = self.coco.loadAnns(ann_ids)
+            # cat_ids = self.coco.getCatIds()
+            # cats = self.coco.loadCats(cat_ids)
+            # paste_masks = np.zeros((image_infos["height"], image_infos["width"]))
+            # anns = sorted(anns, key=lambda idx: idx["area"], reverse=True)
+            # paste_bboxes = []
+            # for i in range(len(anns)):
+            #     category_id = anns[i]["category_id"]
+            #     className = get_classname(category_id, cats)
+            #     pixel_value = self.category_names.index(className)
+            #     x, y, x2, y2 = anns[i]["bbox"]
+            #     paste_bboxes.append([x, y, x2, y2, category_id, i])
+            #     paste_masks[self.coco.annToMask(anns[i]) == 1] = pixel_value
+            # paste_masks = paste_masks.astype(np.int8)
+
+            # transform -> albumentations 라이브러리 활용
+            if self.transform is not None:
+                transformed = self.transform(image=images, mask=masks, bboxes=bboxes)
+
+            return transformed
 
         if self.mode == "test":
             # transform -> albumentations 라이브러리 활용
